b6a88dc37ccf6a73666ffcaec282c8662ae15ffa
JakeThomas7 <131414634+JakeThomas7@users.noreply.github.com>
2025-04-14 20:50:43 -0600

    test

--- backend/Books.API/obj/Release/net8.0/Books.API.AssemblyInfo.cs
+++ backend/Books.API/obj/Release/net8.0/Books.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451d876abac27a2e528c5ef558a893f26d774460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debbf6964b0663709cf67a13210ddf416158b1a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

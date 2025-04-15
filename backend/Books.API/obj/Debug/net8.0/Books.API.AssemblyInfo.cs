d3d9cc296d28721c251f7c65fa4b9df3bc5a7746
JakeThomas7 <131414634+JakeThomas7@users.noreply.github.com>
2025-04-14 20:26:11 -0600

    redeploying bc I deleted the resource

--- backend/Books.API/obj/Debug/net8.0/Books.API.AssemblyInfo.cs
+++ backend/Books.API/obj/Debug/net8.0/Books.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c882426ca40c0030db3af106d1fefa51367968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7512df3202be29acdf5f8e896ff87cef731c0c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Books.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
